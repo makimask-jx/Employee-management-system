532f93c5394a32ad3bfce97e67691ef1bf05f112
Jinx <jinxynet@duck.com>
2026-02-16 16:22:29 +0100

    feat: Parent class defined

---
+++ empleado.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class Empleado(ABC):
+    def __init__(self, nombre: str, salario_base: float) -> None:
+        self.nombre = nombre
+        self.salario_base = salario_base
+
+    @abstractmethod
+    def calcular_salario_mensual(self) -> float:
+        pass
+
+    @abstractmethod
+    def mostrar_informacion(self):
+        pass
